9f93dc31dd503e330a1982ce85577b08c59c9eb9
moralesrmarco <marco2003_6@hotmail.com>
2026-09-20 07:42:51 -0600

    Se estan creando las opciones del menu

--- NorthwindTradersV9WebMVC/wwwroot/js/site.js
+++ NorthwindTradersV9WebMVC/wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.dropdown-submenu > a').forEach(function (element) {
+        element.addEventListener('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            let submenu = this.nextElementSibling;
+            if (submenu) {
+                submenu.classList.toggle('show');
+            }
+        });
+    });
+});
+
+// Cuando la página vuelve desde el historial
+window.addEventListener("pageshow", function () {
+
+    document.querySelectorAll(".action-btn").forEach(btn => {
+
+        btn.classList.remove("disabled");
+
+        btn.removeAttribute("disabled");
+
+        btn.style.pointerEvents = "auto";
+
+    });
+
+});
