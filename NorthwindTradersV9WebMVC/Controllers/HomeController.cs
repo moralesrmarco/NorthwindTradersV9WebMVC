55d0d23c11d5d334f99d1f01208ba2eeaa9a37b6
moralesrmarco <marco2003_6@hotmail.com>
2026-09-22 21:51:10 -0600

    Se corrigio en el homecontroler porque habia eliminado el metodo index

--- NorthwindTradersV9WebMVC/Controllers/HomeController.cs
+++ NorthwindTradersV9WebMVC/Controllers/HomeController.cs
@@ -1,17 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using NorthwindTradersV9BLL;
-using NorthwindTradersV9WebMVC.Models;
 
 namespace NorthwindTradersV9WebMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly MensajeBLL _mensajeBLL;
-        private readonly EmpleadoBLL _empleadoBLL;
-        public HomeController(MensajeBLL mensajeBLL, EmpleadoBLL empleadoBLL)
+        public HomeController()
         {
-            _mensajeBLL = mensajeBLL;
-            _empleadoBLL = empleadoBLL;
+        }
+        public IActionResult Index()
+        {
+            return View();
         }
         public IActionResult Privacy()
         {
